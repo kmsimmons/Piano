f1f9daea692fe49a72dad6e3d8487c93b50cbbc4
kmsimmons <kmsimmons19@gmail.com>
2019-09-12 14:57:28 -0700

    need to fix interactivedemo.js

--- src/App.js
+++ src/App.js
@@ -1,29 +1,61 @@
 import React from 'react';
-import { Piano, KeyboardShortcuts, MidiNumbers } from 'react-piano';
-import 'react-piano/dist/styles.css'
+import 'react-piano/dist/styles.css';
 
-function App() {
-  const firstNote = MidiNumbers.fromNote('c3');
-  const lastNote = MidiNumbers.fromNote('f5');
-  const keyboardShortcuts = KeyboardShortcuts.create({
-    firstNote: firstNote,
-    lastNote: lastNote,
-    keyboardConfig: KeyboardShortcuts.HOME_ROW,
-  });
+import InteractiveDemo from './InteractiveDemo';
+import PlaybackDemo from './PlaybackDemo';
+import { lostWoods } from './songs';
+import './App.css';
 
+const audioContext = new (window.AudioContext || window.webkitAudioContext)();
+const soundfontHostname = 'https://d1pzp51pvbm36p.cloudfront.net';
+
+function Installation() {
   return (
-    <Piano
-      noteRange={{ first: firstNote, last: lastNote }}
-      playNote={(midiNumber) => {
-        // Play a given note - see notes below
-      }}
-      stopNote={(midiNumber) => {
-        // Stop playing a given note - see notes below
-      }}
-      width={1000}
-      keyboardShortcuts={keyboardShortcuts}
-    />
+    <div className="text-center">
+      <h2>Installation</h2>
+      <p className="mt-4">Install with yarn or npm:</p>
+      <p className="mt-3">
+        <code className="p-2 text-dark bg-yellow">yarn add react-piano</code>
+      </p>
+      <div className="mt-5">
+        <a className="btn btn-info btn-lg" href="https://github.com/kevinsqi/react-piano">
+          View docs on Github
+        </a>
+      </div>
+    </div>
   );
+}
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <div className="container">
+          <div className="row mt-5">
+            <div className="col-md-8 offset-md-2">
+              <InteractiveDemo audioContext={audioContext} soundfontHostname={soundfontHostname} />
+            </div>
+          </div>
+          <hr className="mt-5" />
+          <div className="row mt-5">
+            <div className="col-md-8 offset-md-2">
+              <PlaybackDemo
+                audioContext={audioContext}
+                soundfontHostname={soundfontHostname}
+                song={lostWoods}
+              />
+            </div>
+          </div>
+          <hr className="mt-5" />
+          <div className="row mt-5">
+            <div className="col">
+              <Installation />
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
